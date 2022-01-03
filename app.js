a28e9c5fedf11cf9ad7a2ac8865a2b7adedc598a
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-01-03 20:04:30 +0100

    sök

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let weather = {
 
     apiKey: "0a519848631048f8abeeeea37c90ef42",
+
     //fetch funktionen
     fetchData: function (city) {
         //city används som dynamisk parameter, apiKey är definierad ovan och &lang=sv för svensk version.
@@ -9,11 +10,30 @@ let weather = {
         .then((response) => response.json())
         .then((data) => this.showData(data)); //använder funktionen showData och passar datan från API:et som parameter.
     },
+
     showData: function (data) {
         //distructar värden från json datan för att använda det senare.
-        const { description } = data.data[0].weather;
+        const { description, icon } = data.data[0].weather;
+        const { city_name, temp, wind_spd, rh } = data.data[0];
+        //Lägg in värden i Html 
+        document.querySelector(".city").innerText = "Vädret i " + city_name;
+        document.querySelector(".decription").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".weather-icon").src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+        document.querySelector(".wind-speed").innerText = "Vindhastighet: " + wind_spd + "m/s";
+        document.querySelector(".fukt").innerText = "Fuktighet: " + rh + "%";
+    },
 
-        console.log(description);
+    //aktivera sök
+    search: function () {
+        this.fetchData(document.querySelector(".search-input").value);
     }
 
-}
+};
+
+
+//Sök vid click av sök-button.
+document.querySelector(".search-btn").addEventListener("click", ()=>{
+    weather.search();
+    document.querySelector(".search-input").value = "";
+});
